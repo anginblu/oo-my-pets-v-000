00e31b3b2ae628a79a720ba7a0547de13f24d1c6
Jacqueline Lee <anginblu@gmail.com>
2017-11-28 06:40:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,7 +32,7 @@ class Owner
   end
 
   def buy_fish(fish_name)
-    pets[:fishes] = Fish.new(fish_name)
+    pets[:fishes] = nil? pets[:fishes] = Fish.new(fish_name) : pets[:fishes] < Fish.new(fish_name)
   end
 
   def buy_cat(cat_name)
