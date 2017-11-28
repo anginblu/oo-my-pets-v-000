4f55b198365a84b13e5b44a658cb0169adf61258
Jacqueline Lee <anginblu@gmail.com>
2017-11-28 06:28:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,7 +58,7 @@ class Owner
   def list_pets
     pets_number = []
     pets.each do {|type, pet|}
-      pets_number << [type.downcase, "#{pet.size}"]
+      pets_number << [type.to_s.downcase, "#{pet.size}"]
 
   end
 
